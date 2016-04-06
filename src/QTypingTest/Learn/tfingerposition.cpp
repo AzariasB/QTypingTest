ce8ce12fc8f644d70b8c9b4fb440f21a51a20a94
Azarias <azarias.boutin@gmail.com>
2016-04-06 16:38:35 +0100

    Added explanations for the learning dialogs + virtual keyboard'

--- src/QTypingTest/Learn/tfingerposition.cpp
+++ src/QTypingTest/Learn/tfingerposition.cpp
@@ -14,64 +14,64 @@
 #include "tfingerposition.h"
 
 TFingerPosition::TFingerPosition(QWidget* parent) :
-QWidget(parent),
-activeFingers_(QList<int>()) {
-    setFixedSize(400,200);
-    initPoints();
-}
+	QWidget(parent),
+	activeFingers_(QList<int>()) {
+		setFixedSize(400,200);
+		initPoints();
+	}
 
 void TFingerPosition::initPoints() {
-    initRightHand();
+	initRightHand();
 }
 
 
 void TFingerPosition::initRightHand() {
-    //Size of image/2 = 800/2 = 400
-    int size = fingersPoints_.size();
-    for (int i = size - 1; i >= 0; i--) {
-        QVector<QPoint> origin = fingersPoints_[i];
-        QVector<QPoint> points;
-        for (int j = 0; j < origin.size(); j++) {
-            points.append(getSymmetric(origin[j]));
-        }
-        fingersPoints_.append(points);
-    }
+	//Size of image/2 = 800/2 = 400
+	int size = fingersPoints_.size();
+	for (int i = size - 1; i >= 0; i--) {
+		QVector<QPoint> origin = fingersPoints_[i];
+		QVector<QPoint> points;
+		for (int j = 0; j < origin.size(); j++) {
+			points.append(getSymmetric(origin[j]));
+		}
+		fingersPoints_.append(points);
+	}
 }
 
 QPoint TFingerPosition::getSymmetric(const QPoint& origin) const {
-    int middleImage = 400;
-    QPoint point;
-    point.setX(origin.x() + ((middleImage - origin.x()) << 1));
-    point.setY(origin.y());
-    return point;
+	int middleImage = 400;
+	QPoint point;
+	point.setX(origin.x() + ((middleImage - origin.x()) << 1));
+	point.setY(origin.y());
+	return point;
 }
 
 void TFingerPosition::disableFinger(FINGER id) {
-    if (activeFingers_.contains(id) && id < NO_FINGER ) {
-        activeFingers_.removeAll(id);
-    }
-    update();
+	if (activeFingers_.contains(id) && id < NO_FINGER ) {
+		activeFingers_.removeAll(id);
+	}
+	update();
 }
 
 void TFingerPosition::enableFinger(FINGER id) {
-    if (!activeFingers_.contains(id) && id < NO_FINGER ) {
-        activeFingers_.append(id);
-    }
-    update();
+	if (!activeFingers_.contains(id) && id < NO_FINGER ) {
+		activeFingers_.append(id);
+	}
+	update();
 }
 
 void TFingerPosition::paintEvent(QPaintEvent*) {
-    QPainter painter(this);
-    painter.scale(0.5, 0.5);
+	QPainter painter(this);
+	painter.scale(0.5, 0.5);
 
-    QPainterPath tmpPath;
-    for (auto elem : activeFingers_) {
-        QVector<QPoint> pol = fingersPoints_[elem];
-        QPolygon p(pol);
-        tmpPath.addPolygon(p);
-    }
-    painter.fillPath(tmpPath, QBrush(Qt::blue));
+	QPainterPath tmpPath;
+	for (auto elem : activeFingers_) {
+		QVector<QPoint> pol = fingersPoints_[elem];
+		QPolygon p(pol);
+		tmpPath.addPolygon(p);
+	}
+	painter.fillPath(tmpPath, QBrush(Qt::blue));
 
-    QImage image(":/images/hands.png");
-    painter.drawImage(image.rect(), image);
+	QImage image(":/images/hands.png");
+	painter.drawImage(image.rect(), image);
 }
